0b3297d37bb06267bc32de49a40aa6764096d8e0
Re1del <r31del@gmail.com>
2014-03-06 15:28:16 +0100

    Update ClaseLexica.java
    Añadidos comentarios a las Clases Léxicas

--- src/alex/manual/ClaseLexica.java
+++ src/alex/manual/ClaseLexica.java
@@ -1,14 +1,29 @@
 package alex.manual;
 
-
+/*
+DEFINICION DE CLASES LÉXICAS
+*/
 public enum ClaseLexica {
+	/*CLs de comparadores != == > >= < <=*/
 	NEQ, EQ, GT, EGT, LT, ELT,
-	IDEN, ENT, PAP, 
-	PCIERRE, PUNTOCOMA, 
+	/*CL Indentificador*/
+	IDEN,
+	/*CL Entero*/
+	ENT,
+	/*CLs de Parentesis de Apertura y Cierre*/
+	PAP, PCIERRE, 
+	/*CL Punto  y Coma ;*/
+	PUNTOCOMA, 
+	/*CLs de Operaciones + - * / */
 	MAS, MENOS, POR, DIV, 
+	/*CL Separador de Secciones && */
 	ASIG, SEPSEC, 
+	/*CLs de Operaciones Booleanas*/
 	AND, OR, NOT, 
+	/*CLs de Nombres de Clases*/
 	BOOL, INT,
+	/*CLs de Valores Booleanos*/
 	TRUE, FALSE,
+	/*CL Fin de Fichero*/
 	EOF 
 }
